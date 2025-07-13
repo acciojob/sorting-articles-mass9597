76195c4a85dd272412c5e0793bd7b8bdeb58db9d
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-13 21:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function trimmed(name){
+	return name.replace(/^(a |an |the )/i,"").trim();
+}
+const copies = bands.slice().sort((a,b) =>
+	trimmed(a).localeCompare(trimmed(b))
+	
+);
+
+const ul = document.getElementById("band");
+
+copies.forEach( copy => {
+	const list = document.createElement("li");
+    list.textContent = copy;
+    ul.appendChild(list);
+});
+
